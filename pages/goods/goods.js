7fe8efbe5ca7f19dd2096555c09264f1a3c24d29
tongyun,zhang <tongyun.zhang@chinaredstar.com>
2018-08-24 16:02:46 +0800

    提交

---
+++ pages/goods/goods.js
@@ -0,0 +1,61 @@
+Page({
+  data: {
+    goodsList: []
+  },
+  onLoad: function (options) {
+    // 生命周期函数--监听页面加载
+    console.log(options)
+    var ta = this;
+    wx.request({
+      url: 'http://localhost:7725/productGoods/getList',
+      header: {
+        'content-type': 'application/json'
+      },
+      method: 'POST',
+      data: {
+        page: 1,
+        limit: 15,
+        categoryId: options.id
+      },
+      success: function (res) {
+        ta.setData(
+          {
+            goodsList: res.data.rows
+          }
+        )
+      }
+    })
+  },
+  onReady: function () {
+    // 生命周期函数--监听页面初次渲染完成
+
+  },
+  onShow: function () {
+    // 生命周期函数--监听页面显示
+
+  },
+  onHide: function () {
+    // 生命周期函数--监听页面隐藏
+
+  },
+  onUnload: function () {
+    // 生命周期函数--监听页面卸载
+
+  },
+  onPullDownRefresh: function () {
+    // 页面相关事件处理函数--监听用户下拉动作
+
+  },
+  onReachBottom: function () {
+    // 页面上拉触底事件的处理函数
+
+  },
+  onShareAppMessage: function () {
+    // 用户点击右上角分享
+    return {
+      title: 'title', // 分享标题
+      desc: 'desc', // 分享描述
+      path: 'path' // 分享路径
+    }
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,67 +4,111 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    // motto: 'Hello World',
+    // userInfo: {},
+    // hasUserInfo: false,
+    // canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    banners: [], //初始化一个 banners 数组
+    shelfNavList:[],
+    shelf: {
+      english_name: "New Arrivals",
+      name: "新品上架"
+    },
+    productNewList:[],
+    //以下四项设置 swiper 组件的参数值
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    circular: true,
+    interval: 2000,
+    duration: 500
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+  // //事件处理函数
+  // bindViewTap: function() {
+  //   wx.navigateTo({
+  //     url: '../logs/logs'
+  //   })
+  // },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    // wx.authorize({ scope: "scope.userInfo" })
+    // if (app.globalData.userInfo) {
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //     hasUserInfo: true
+    //   })
+    // } else if (this.data.canIUse){
+    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //   // 所以此处加入 callback 以防止这种情况
+    //   app.userInfoReadyCallback = res => {
+    //     this.setData({
+    //       userInfo: res.userInfo,
+    //       hasUserInfo: true
+    //     })
+    //   }
+    // } else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   wx.getUserInfo({
+    //     success: res => {
+    //       app.globalData.userInfo = res.userInfo
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //         hasUserInfo: true
+    //       })
+    //     }
+    //   })
+    // }
 
-    var that = this;
+    var ta = this;
     wx.request({
-      url: 'http://localhost:7725/user/getUser',
+      url: 'http://localhost:7725/productGoods/getList',
       header: {
         'content-type': 'application/json'
       },
+      method:'POST',
+      data:{
+        page:1,
+        limit:15
+      },
       success: function (res) {
         console.log(res.data)
-        that.setData(
+        ta.setData(
           {
-            uname: res.data
+            banners: res.data.rows,
+            productNewList: res.data.rows
           }
         )
+        console.log(ta.data)
+      }
+    })
+
+    wx.request({
+      url: 'http://localhost:7725/showCategory/getLevOne',
+      header: {
+        'content-type': 'application/json'
+      },
+      method: 'POST',
+      data: {
+        page: 1,
+        limit: 5
+      },
+      success: function (res) {
+        console.log(res.data)
+        ta.setData(
+          {
+            shelfNavList: res.data.rows
+          }
+        )
+        console.log(ta.data)
       }
     })
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+  // getUserInfo: function(e) {
+  //   console.log(e)
+  //   app.globalData.userInfo = e.detail.userInfo
+  //   this.setData({
+  //     userInfo: e.detail.userInfo,
+  //     hasUserInfo: true
+  //   })
+  // }
+  
 })
